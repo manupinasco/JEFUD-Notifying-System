3f6bbcbf04a99763f932f11ba18571b7081b7dd9
Uriel1621 <uriel.fischman@gmail.com>
2022-10-08 18:34:00 -0300

    Navegacion de login entre graphs

--- app/src/main/java/ar/edu/ort/jefud_notifying_system/LoginFragment.kt
+++ app/src/main/java/ar/edu/ort/jefud_notifying_system/LoginFragment.kt
@@ -1,11 +1,14 @@
 package ar.edu.ort.jefud_notifying_system
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import ar.edu.ort.jefud_notifying_system.panelista.PanelistaActivity
 
 
 class LoginFragment : Fragment() {
@@ -16,5 +19,17 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(v: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(v, savedInstanceState)
+        val buttonLogin = v.findViewById<Button>(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener{
+            //if(user.rol == "panelista"){
+            startActivity(Intent(requireContext(), PanelistaActivity::class.java))
+            //}
+        }
+
     }
 }

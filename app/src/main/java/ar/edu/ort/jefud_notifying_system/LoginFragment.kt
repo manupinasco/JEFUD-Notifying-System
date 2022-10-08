dc59d7ef35f8cded63657f60c0d1d88e914289b1
Uriel1621 <uriel.fischman@gmail.com>
2022-10-08 19:11:33 -0300

    Alerta cuando los campos estan vacios

--- app/src/main/java/ar/edu/ort/jefud_notifying_system/LoginFragment.kt
+++ app/src/main/java/ar/edu/ort/jefud_notifying_system/LoginFragment.kt
@@ -7,11 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import ar.edu.ort.jefud_notifying_system.panelista.PanelistaActivity
 
 
 class LoginFragment : Fragment() {
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,12 +28,20 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(v: View, savedInstanceState: Bundle?) {
         super.onViewCreated(v, savedInstanceState)
         val buttonLogin = v.findViewById<Button>(R.id.buttonLogin)
+        val editTextDNI = v.findViewById<EditText>(R.id.editTextDNI)
+        val editTextPassword = v.findViewById<EditText>(R.id.editTextPassword)
 
-        buttonLogin.setOnClickListener{
-            //if(user.rol == "panelista"){
-            startActivity(Intent(requireContext(), PanelistaActivity::class.java))
-            //}
+
+        buttonLogin.setOnClickListener {
+            if (editTextDNI.text.isNullOrBlank() || editTextPassword.text.isNullOrBlank()) {
+                Toast.makeText(getContext(), "Falta rellenar campos", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                //if(user.rol == "panelista"){
+                startActivity(Intent(requireContext(), PanelistaActivity::class.java))
+                //}
+            }
+
         }
-
     }
 }

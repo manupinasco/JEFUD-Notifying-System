ed904a89699c664df577c45c6b1c222126904338
Uriel1621 <uriel.fischman@gmail.com>
2022-10-08 15:56:51 -0300

    Agrego diseño login sin logo raizen

---
+++ app/src/main/java/ar/edu/ort/jefud_notifying_system/panelista/PanelistaActivity.kt
@@ -0,0 +1,12 @@
+package ar.edu.ort.jefud_notifying_system.panelista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ar.edu.ort.jefud_notifying_system.R
+
+class PanelistaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_panelista)
+    }
+}

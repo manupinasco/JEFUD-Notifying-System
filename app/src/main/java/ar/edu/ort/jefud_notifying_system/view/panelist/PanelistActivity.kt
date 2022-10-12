5befe20f3dfa3101c3c0cd0f0173ee27aec60c17
FavioLuna <fdluna99@gmail.com>
2022-10-12 18:20:28 -0300

    metodo setupBottomNav Panelista

--- app/src/main/java/ar/edu/ort/jefud_notifying_system/view/panelist/PanelistActivity.kt
+++ app/src/main/java/ar/edu/ort/jefud_notifying_system/view/panelist/PanelistActivity.kt
@@ -2,8 +2,11 @@ package ar.edu.ort.jefud_notifying_system.view.panelist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ar.edu.ort.jefud_notifying_system.R
 import ar.edu.ort.jefud_notifying_system.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class PanelistActivity : AppCompatActivity() {
 
@@ -13,5 +16,17 @@ class PanelistActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupBottomNavBar()
+
     }
+
+    private fun setupBottomNavBar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_panelist) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.botNavPanelist).setupWithNavController(navController)
+    }
+    
 }
